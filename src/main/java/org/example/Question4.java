272a8a44edfb37780902edf010999d1527d9140f
Dermot Logue <dermot.logue@dkit.ie>
2024-11-29 13:32:45 +0000

    added new starter matrix with walls for floodfill

--- src/main/java/org/example/Question4.java
+++ src/main/java/org/example/Question4.java
@@ -4,51 +4,47 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
- *  Name:
- *  Class Group:
+ * Name:
+ * Class Group:
  */
 
 public class Question4  // Flood Fill (Stack, 2D Array)
 {
+    static final int MAX_ROWS = 5;
+    static final int MAX_COLS = 5;
+
     public static void main(String[] args) {
-        start();
-    }
-    public static void start()
-    {
-        int[][] arr = floodFillStart();
+        System.out.println("Question 4. Floodfill algorithm.");
+
+        // Starter matrix (2D Array) with 0 representing an empty cell,
+        // and -1 representing a wall. Flood fill can not cross through
+        // a wall ( and not pass through diagionally).
+        //
+        int[][] matrix = new int[MAX_ROWS][MAX_COLS]; // 2D Array of int
+        // define values for each row, -1 to prevent change
+        matrix[0] = new int[]{ 0, 0, -1, -1, 0};
+        matrix[1] = new int[]{ 0, 0, -1, -1, 0};
+        matrix[2] = new int[]{-1, 0,  0,  0, 0};
+        matrix[3] = new int[]{-1, 0, -1, -1, 0};
+        matrix[4] = new int[]{ 0,-1, -1,  0, 0};
+
+        display(matrix);
+
     }
 
     /*
-        Starter function to create the 2D array and populate it with zeros
+        Helper function to display the 2D Array
      */
-    public static int[][]  floodFillStart() {
-        Scanner kb = new Scanner(System.in);
-        int[][] arr = new int[10][10];
-        for (int x = 0; x < 10; x++)
-        {
-            for (int y = 0; y < 10; y++)
-            {
-                arr[x][y] = 0;
-            }
-        }
-        return arr;
-    }
-    /*
-        Helper function to display the image
-     */
-    public static void display(int[][] arr)
-    {
-        for (int x = 0; x < 10; x++)
-        {
-            for (int y = 0; y < 10; y++)
-            {
+    public static void display(int[][] arr) {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
                 System.out.printf("%4d", arr[x][y]);
             }
             System.out.println();
         }
     }
-    private static void fill(int r, int c, int[][] arr)
-    {
+
+    private static void floodFill(int r, int c, int[][] arr) {
 
     }
 
