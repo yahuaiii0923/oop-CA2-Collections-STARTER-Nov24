300ccd6a6c68c28bac5c64d099d5f4a17dca492f
harrist03 <152608673+harrist03@users.noreply.github.com>
2024-12-06 11:04:11 +0000

    q3 done

--- src/main/java/org/example/Question3.java
+++ src/main/java/org/example/Question3.java
@@ -1,34 +1,74 @@
 package org.example;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
- *  Name: Cheryl Kong
- *  Class Group: SD2B
+ * Name: Harris Teh Kai Ze
+ * Class Group: SD2B
  */
-public class Question3  {   //Nested HTML (Stack)
+public class Question3 { // Nested HTML (Stack)
 
     /*
-filename: name of the file to test.
-*/
-    public static boolean validate(String filename) throws FileNotFoundException
-    {
-        return false;
+     * filename: name of the file to test.
+     */
+    public static boolean validate(String filename) throws FileNotFoundException {
+        String[] selfClosingTags = { "<br>", "<img>", "<hr>", "<input>", "<meta>", "<link>" };
+
+        Scanner sc = new Scanner(new File(filename));
+        Stack<String> tagStack = new Stack<>();
+
+        while (sc.hasNext()) {
+            String tag = sc.next();
+
+            // Check if it's a self-closing tag
+            boolean isSelfClosing = false;
+            for (String selfClosingTag : selfClosingTags) {
+                if (tag.equals(selfClosingTag)) {
+                    isSelfClosing = true;
+                    break;
+                }
+            }
+            // if it's a self-closing tag, skip the iteration
+            if (isSelfClosing) {
+                continue;
+            }
+            if (tag.startsWith("</")) {
+                if (tagStack.isEmpty()) {
+                    sc.close();
+                    return false;
+                }
+                // pop the top element
+                String lastAccessedTag = tagStack.pop();
+                // </a> != </ + li> invalid example
+                // </a> == </ + a> valid example
+                if (!tag.equals("</" + lastAccessedTag.substring(1))) {
+                    sc.close();
+                    return false;
+                }
+            } else if (tag.startsWith("<") && tag.endsWith(">")) {
+                tagStack.push(tag);
+            }
+        }
+        sc.close();
+        // the stack should be empty if all tags are matched because they have been popped
+        return tagStack.isEmpty();
     }
 
     /*
-        This function tests the files in the files array to see if
-         they are valid.
-         tags_valid.txt should return true;
-         tags_invalid.txt should output as invalid;
-
-
+     * This function tests the files in the files array to see if
+     * they are valid.
+     * tags_valid.txt should return true;
+     * tags_invalid.txt should output as invalid;
+     * 
+     * 
      */
     public static void main(String[] args) throws FileNotFoundException {
-        String[] files = {"tags_valid.txt", "tags_invalid.txt"};
-        for(String fName: files) {
-            System.out.print(fName +": ");
+        String[] files = { "tags_valid.txt", "tags_invalid.txt" };
+        for (String fName : files) {
+            System.out.print(fName + ": ");
             if (validate(fName)) {
                 System.out.println("This file is valid");
             } else {
@@ -37,8 +77,4 @@ filename: name of the file to test.
         }
     }
 
-
-
 }
-
-
